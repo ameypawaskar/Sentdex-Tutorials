5181fc909bb61c469b18820e52d3fa59ac7deef5
Amey Pawaskar <40467868+ameypawaskar@users.noreply.github.com>
2019-03-02 15:55:14 +0530

    Add files via upload

---
+++ Python3 Basic Tutorial/Python3 Basic 1 - Hello World.py
@@ -0,0 +1,3 @@
+print('Hello India!')
+print("Hello World!")
+print("""Hello Universe!""")
---
+++ Python3 Basic Tutorial/Python3 Basic 2 - Tuples, Strings, Loops.py
@@ -0,0 +1,9 @@
+#Tuple
+programming_languages = "Python", "Java", "C++", "C#"
+indian_languages = ("Marathi", "Konkani", "Tamil", "Kannad")
+
+print(type(programming_languages))
+print(type(indian_languages))
+
+for language in programming_languages:
+    print(language)    
